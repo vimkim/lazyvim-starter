a08571aff1cffee651139721473b24cde238b71b
Daehyun Kim <kimdhyungg@gmail.com>
2026-07-22 14:02:18 +0900

    fix treesitter just (for module parsing)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,27 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  init = function()
+    -- Temporary workaround for module::recipe dependencies in justfiles.
+    -- Keep this fork override until the upstream parser fixes:
+    -- https://github.com/casey/tree-sitter-just/issues/206
+    local group = vim.api.nvim_create_augroup("custom_just_parser", { clear = true })
+
+    vim.api.nvim_create_autocmd("User", {
+      group = group,
+      pattern = "TSUpdate",
+      callback = function()
+        require("nvim-treesitter.parsers").just = {
+          install_info = {
+            url = "https://github.com/tkatter/tree-sitter-just",
+            branch = "update-grammar",
+            revision = "912ac5a7b763af3ac488dbfaafb05fde7e926ece",
+            queries = "queries/just",
+          },
+          tier = 2,
+        }
+      end,
+    })
+  end,
   opts = {
     indent = { enable = false }, -- experimental, not work well
     ensure_installed = {
@@ -10,7 +32,6 @@ return {
       "javascript",
       "jsdoc",
       "json",
-      "jsonc",
       "just",
       "lua",
       "luadoc",
