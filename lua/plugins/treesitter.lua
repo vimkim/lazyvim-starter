e7bce87adf103d4da84ce8b743ee48f31e4e0885
Daehyun Kim <kimdhyungg@gmail.com>
2026-07-22 14:23:18 +0900

    temp fix: treesitter just 1.55

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   init = function()
-    -- Temporary workaround for module::recipe dependencies in justfiles.
-    -- Keep this fork override until the upstream parser fixes:
+    -- Temporary local parser for module::recipe dependencies and the Just 1.55
+    -- constructs used by the CUBRID justfiles. Remove this override and its
+    -- stdpath("data") checkout once the upstream parser fixes:
     -- https://github.com/casey/tree-sitter-just/issues/206
     local group = vim.api.nvim_create_augroup("custom_just_parser", { clear = true })
 
@@ -12,9 +13,7 @@ return {
       callback = function()
         require("nvim-treesitter.parsers").just = {
           install_info = {
-            url = "https://github.com/tkatter/tree-sitter-just",
-            branch = "update-grammar",
-            revision = "912ac5a7b763af3ac488dbfaafb05fde7e926ece",
+            path = vim.fn.stdpath("data") .. "/tree-sitter-just-workaround",
             queries = "queries/just",
           },
           tier = 2,
